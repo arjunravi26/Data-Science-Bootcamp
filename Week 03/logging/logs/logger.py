faf74ed9912b165cfd80470165405aa178151a99
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-01-03 15:06:11 +0530

    log

---
+++ Week 03/logging/logs/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(
+    filename='app.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s-%(levelname)s-%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+    )
