c58306a0c38916b9de6f1711ec2593bfb7675be6
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-01-07 14:40:57 +0530

    bst

--- Week 20/DSA/Tree/bst.py
+++ Week 20/DSA/Tree/bst.py
@@ -30,11 +30,46 @@ class BST:
             print(f"{value} is already present in the tree")
             return
 
-    def delete(self):
-        pass
+    def delete(self, value):
+        if not self.root:
+            return False
+        self._delete(self.root, value)
+
+    def _delete(self, node, value):
+        if node is None:
+            return False
+        if node.value > value:
+            node.left = self._delete(node.left, value)
+        elif node.value < value:
+            node.right = self._delete(node.right, value)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            else:
+                min_node = self.find_min(node)
+                node.value == min_node.value
+                self._delete(node.right, min_node.value)
+        return node
 
     def search(self, value):
-        pass
+        if not self.root:
+            return None
+        node = self.root
+        while node:
+            if node.value == value:
+                return node.value
+            elif node.value > value:
+                node = node.left
+            else:
+                node = node.right
+        return None
+
+    def find_min(self, node):
+        if node.left is None:
+            return node
+        self.find_min(node.left)
 
     def inorder(self):
         if not self.root:
@@ -88,3 +123,6 @@ tree.insert(8)
 tree.inorder()
 tree.preorder()
 tree.postorder()
+tree.delete(30)
+tree.postorder()
+print(tree.search(5))
