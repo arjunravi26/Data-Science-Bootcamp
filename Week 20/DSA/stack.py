bb10b2904e0402f40973163af4a714c50558692c
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-01-11 11:01:33 +0530

    stack

---
+++ Week 20/DSA/stack.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def search(self, value):
+        pass
+
+    def overflow(self):
+        pass
+
+    def undeflow(self):
+        pass
