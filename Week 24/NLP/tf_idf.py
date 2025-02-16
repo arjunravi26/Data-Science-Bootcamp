753af095db3d16740e6a909e942e77309d4f5c8c
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-02-16 12:49:45 +0530

    tf-idf

--- Week 24/NLP/tf_idf.py
+++ Week 24/NLP/tf_idf.py
@@ -25,7 +25,7 @@ print(vocab)
 tf_freq = []
 for sentence in sentences:
     words = nltk.word_tokenize(sentence)
-    tf_freq.append([word.count(word) / len(words) for word in vocab])
+    tf_freq.append([words.count(word) / len(words) for word in vocab])
 tf_freq = np.array(tf_freq)
 print(tf_freq)
 
@@ -44,7 +44,7 @@ print(idf)
 tf_idf = tf_freq * idf
 print(tf_idf)
 
+index=[f'doc{i+1}' for i in range(len(sentences))]
 df = pd.DataFrame(data=tf_idf, columns=list(
-    vocab), index=['doc1', 'doc2', 'doc3'])
+    vocab), index=index)
 print(df)
-df.to_csv('tf_idf.csv')
