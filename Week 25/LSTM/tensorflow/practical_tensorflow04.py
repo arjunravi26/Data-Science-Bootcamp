c05cc627a580c33687e6b106271bdce160c169e0
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-03-04 21:08:35 +0530

    practice

--- Week 25/LSTM/tensorflow/practical_tensorflow04.py
+++ Week 25/LSTM/tensorflow/practical_tensorflow04.py
@@ -3,17 +3,28 @@ import tensorflow as tf
 X = tf.constant(5)
 print(X.numpy())
 
-a = tf.constant([[1, 2],[3, 4]])
-b = tf.constant([[1, 1],[1, 1]])
+a = tf.constant([[1, 2], [3, 4]])
+b = tf.constant([[1, 1], [1, 1]])
 
 print(tf.add(a, b), "\n")
 print(tf.multiply(a, b), "\n")
 print(tf.matmul(a, b), "\n")
 
 
-print(a + b, "\n") # element-wise addition
-print(a * b, "\n") # element-wise multiplication
-print(a @ b, "\n") # matrix multiplication
+print(a + b, "\n")  # element-wise addition
+print(a * b, "\n")  # element-wise multiplication
+print(a @ b, "\n")  # matrix multiplication
 
-a = tf.convert_to_tensor([1,2,3])
+a = tf.convert_to_tensor([1, 2, 3])
 print(a)
+
+# rank of matrix(dimension)
+rank_matrix = tf.Variable([[[1, 2, 3]]])
+print(rank_matrix)
+print(tf.rank(rank_matrix))
+
+rank_matrix.assign([[[10, 15, 16]]])
+print(rank_matrix)
+
+rank_matrix.assign_add([[[3, 4, 0]]])
+print(rank_matrix)
